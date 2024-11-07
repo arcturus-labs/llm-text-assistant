19cd2e207b465063f302b1b2ca25275904101adc
John Berryman <jfberryman@gmail.com>
2024-11-06 22:36:35 -0600

    fake out frontend build in backend tests

--- scripts/run-backend-tests.sh
+++ scripts/run-backend-tests.sh
@@ -8,6 +8,14 @@ echo "Installing backend dependencies..."
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+# Ensure (or fake) frontend is built
+cd ../frontend
+if [ ! -d "dist" ]; then
+    mkdir dist
+    echo "HELLO WORLD" > dist/index.html
+fi
+cd ../backend
+
 # Run tests with correct PYTHONPATH
 echo "Running backend tests..."
 PYTHONPATH=$PYTHONPATH:$(pwd)/.. pytest -v
